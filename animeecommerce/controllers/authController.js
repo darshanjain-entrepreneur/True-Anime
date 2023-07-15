edcef35385f120221631ab9f05aa45b56eda48bd
Darshan Jain <darshanjain.godha@gmail.com>
2023-07-15 16:57:00 +0530

    added the route and login for login also add token feature

--- animeecommerce/controllers/authController.js
+++ animeecommerce/controllers/authController.js
@@ -1,5 +1,6 @@
 import userModels from "../models/userModels.js";
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
   try {
@@ -51,6 +52,60 @@ export const registerController = async (req, res) => {
     return res.status(500).send({
       success: false,
       message: "Error in registration",
+      error,
+    });
+  }
+};
+
+// login controller
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const user = await userModels.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not Registered",
+      });
+    }
+
+    const match = await comparePassword(password, user.password);
+
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    return res.status(201).send({
+      success: true,
+      message: "Login successful",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error in Login",
       error,
     });
   }
