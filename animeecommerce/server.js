ae778b62fbcd33f15207c884e98d1480ecfc3a62
Darshan Jain <darshanjain.godha@gmail.com>
2023-07-15 15:28:23 +0530

    implemented the schema for users in database

--- animeecommerce/server.js
+++ animeecommerce/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
+import router from "./routes/authRoute.js";
 import morgan from "morgan";
 
 dotenv.config();
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("api/v1/auth", router);
 
 app.get("/", (req, res) => {
   res.send({
