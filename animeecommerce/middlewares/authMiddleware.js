2b93d3e2287398a915f8d8beb1d72e914f557785
Darshan Jain <darshanjain.godha@gmail.com>
2023-07-15 18:21:46 +0530

    added the middleware to stop user to access admin

--- animeecommerce/middlewares/authMiddleware.js
+++ animeecommerce/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import JWT from "jsonwebtoken";
+import userModels from "../models/userModels.js";
 
 export const reqireSignIn = async (req, res, next) => {
   try {
@@ -6,8 +7,32 @@ export const reqireSignIn = async (req, res, next) => {
       req.headers.authorization,
       process.env.JWT_SECRET
     );
+    req.user = decode;
     next();
   } catch (error) {
     console.log(error);
+  }
+};
+
+// admin access
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userModels.findById(req.user._id);
+    if (user.role !== 1) {
+      return res.status(401).send({
+        success: false,
+        message: "UnAuthorized access",
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "error in admin middleware",
+      error,
+    });
   }
 };
