b060d1ff0bb598d1194bbb775b3c9c2bb526e248
Darshan Jain <darshanjain.godha@gmail.com>
2023-07-17 14:27:16 +0530

    fixed some bugs on libray outdated and added routin

---
+++ animeecommerce/client/src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+   <h4 className='text-center'>
+   ALL RIGHTS To &copy; Darshan Jain
+   </h4>
+    </div>
+  )
+}
+
+export default Footer
---
+++ animeecommerce/client/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from "../Layout/Header"
+import Footer from "../Layout/Footer"
+
+const Layout = (props) => {
+  return (
+ <div>
+<Header/>
+
+<main  style={{minHeight:'80vh'}} >{props.children}</main>
+
+<Footer/>
+ </div>
+  )
+}
+
+export default Layout
